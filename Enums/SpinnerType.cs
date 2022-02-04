12db8d39754398e8fad7cdb45d8fc20885341c02
Prathamesh Shende <prathamesh.a4@gmail.com>
2022-02-04 23:04:28 +0530

    doc: Added Tetbox select and update components. fixed project error due to page and view

---
+++ Enums/SpinnerType.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.SmartEnum;
+namespace BootstrapComponents.Enums;
+public sealed class SpinnerType : SmartEnum<SpinnerType>
+{
+    public static readonly SpinnerType Border = new("border", 1);
+    public static readonly SpinnerType Grow = new("grow", 2);
+
+
+    public SpinnerType(string name, int value) : base(name, value)
+    {
+    }
+}
+
