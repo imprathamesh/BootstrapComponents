36fde14a807046e9bcad55f592550835e89356b6
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2023-06-09 17:04:16 +0530

    code cleanup

--- Enums/Direction.cs
+++ Enums/Direction.cs
@@ -8,7 +8,7 @@ namespace BootstrapComponents.Enums
         public static readonly Direction End = new("end", 2);
         public static readonly Direction Top = new("top", 3);
         public static readonly Direction Bottom = new("bottom", 4);
-        
+
         public Direction(string name, int value) : base(name, value)
         {
         }
