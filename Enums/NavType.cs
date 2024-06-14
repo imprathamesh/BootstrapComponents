6a1f62c730c7e4c52b4d9cf51f8949f688f5666b
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2024-06-14 14:21:42 +0530

    Fix Avatar Added Table Pagination Class Update NavType

--- Enums/NavType.cs
+++ Enums/NavType.cs
@@ -7,6 +7,7 @@ namespace BootstrapComponents.Enums
     {
         public static readonly NavType Pill = new("pills", 1);
         public static readonly NavType Tab = new("tabs", 2);
+        public static readonly NavType Underline = new("underline", 3);
 
 
         public NavType(string name, int value) : base(name, value)
---
+++ Pagination/TablePagination.cs
@@ -0,0 +1,8 @@
+﻿namespace BootstrapComponents.Pagination;
+public class TablePagination
+{
+    public int CurrentPage { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public int Total { get; set; } = 0;
+    public int Length { get => Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(Total) / PageSize)); }
+}
