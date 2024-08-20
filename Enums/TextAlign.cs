f4abaf8f3098ee730fef3245170536a89bee5da8
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2024-08-20 17:06:06 +0530

    refactor

--- Enums/TextAlign.cs
+++ Enums/TextAlign.cs
@@ -2,9 +2,9 @@
 namespace BootstrapComponents.Enums;
 public sealed class TextAlign : SmartEnum<TextAlign>
 {
-    public static readonly TextAlign Start = new("start", 1);
-    public static readonly TextAlign Center = new("center", 2);
-    public static readonly TextAlign End = new("end", 3);
+    public static readonly TextAlign Start = new("text-start", 1);
+    public static readonly TextAlign Center = new("text-center", 2);
+    public static readonly TextAlign End = new("text-end", 3);
 
 
     public TextAlign(string name, int value) : base(name, value)
