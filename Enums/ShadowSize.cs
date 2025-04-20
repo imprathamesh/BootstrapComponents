114480a20d7c0f666369824b0f9899f39306762b
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2025-04-20 06:29:16 +0530

    Search box Fixed Tableth has sort

--- Enums/ShadowSize.cs
+++ Enums/ShadowSize.cs
@@ -3,10 +3,10 @@ namespace BootstrapComponents.Enums;
 
 public sealed class ShadowSize : SmartEnum<ShadowSize>
 {
-    public static readonly ShadowSize Small = new ShadowSize("sm", 1);
-    public static readonly ShadowSize Medium = new ShadowSize("md", 2);
-    public static readonly ShadowSize Large = new ShadowSize("lg", 3);
-    public static readonly ShadowSize None = new ShadowSize("none", 4);
+    public static readonly ShadowSize Small = new("sm", 1);
+    public static readonly ShadowSize Medium = new("md", 2);
+    public static readonly ShadowSize Large = new("lg", 3);
+    public static readonly ShadowSize None = new("none", 4);
     public ShadowSize(string name, int value) : base(name, value)
     {
     }
