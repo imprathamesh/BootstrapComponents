cd28660a17554188cd8741518fae6cb2a3ebbe2d
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2026-06-23 06:52:00 +0530

    Added Doughnut Chart

---
+++ Charts/DoughnutSegment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapComponents.Charts;
+
+public class DoughnutSegment
+{
+    public string Label { get; set; } = string.Empty;
+
+    public decimal Value { get; set; }
+
+    public string Color { get; set; } = "#0d6efd";
+}
